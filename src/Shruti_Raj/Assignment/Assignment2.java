db2880bfeae666b54adc7992398ba06316363960
Shruti raj <shrutiraj7594@gmail.com>
2026-02-06 15:44:19 +0530

    Assignment2,3 is submitted

---
+++ src/Shruti_Raj/Assignment/Assignment2.java
@@ -0,0 +1,18 @@
+package Shruti_Raj.Assignment;
+
+public class Assignment2 {
+	public static void main(String[] args) {
+		int  num1 = 10;
+		int num2 = 2;
+		
+		int add = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		int div = num1 / num2;
+		
+		int total = add + sub + mul + div;
+		System.out.println("The answer is"+total);
+		
+	}
+
+}
