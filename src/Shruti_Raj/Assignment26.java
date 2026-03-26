7b708e369a19617ef6fa96fd3e6889d199e99385
Shruti raj <shrutiraj7594@gmail.com>
2026-03-26 15:21:20 +0530

    Assignment26 submitted

---
+++ src/Shruti_Raj/Assignment26.java
@@ -0,0 +1,16 @@
+package Shruti_Raj;
+
+public class Assignment26 {
+	static int getAscii(char ch) {
+		
+		return(int) ch;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(getAscii('A'));
+		System.out.println(getAscii('X'));
+		System.out.println(getAscii(' '));
+		System.out.println(getAscii('.'));
+	}
+
+}
