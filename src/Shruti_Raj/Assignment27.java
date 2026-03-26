357a0d0dc33b63faf92d3b616ffb013b9c04cc85
Shruti raj <shrutiraj7594@gmail.com>
2026-03-26 15:31:30 +0530

    Assignment27 completed

---
+++ src/Shruti_Raj/Assignment27.java
@@ -0,0 +1,14 @@
+package Shruti_Raj;
+
+public class Assignment27 {
+	static char getChar(int ascii) {
+		return (char) ascii;
+	}
+	
+	public static void main (String []args) {
+		System.out.println(getChar(65));
+		System.out.println(getChar(90));
+		System.out.println(getChar(55));
+	}
+
+}
